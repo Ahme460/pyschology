ade17324b799724d1468ad840f29069aebde34ef
Ahme460 <ahmeoon1234@gmail.com>
2024-04-02 22:36:15 +0200

    new

--- app2/static/js/blog.js
+++ app2/static/js/blog.js
@@ -1,47 +1,30 @@
-let inp_img =document.getElementById("image")
-let inp_vid =document.getElementById("video")
-let showvideo =document.getElementById("showvideo")
-let showimg =document.getElementById("showimg")
+let inp_img = document.getElementById("image");
+let showimg = document.getElementById("showimg");
+let but = document.getElementById("save");
 
+but.onclick = function() {
+  let fr = new FileReader();
+  fr.readAsDataURL(inp_img.files[0]);
+  fr.onload = function() {
+    showimg.src = fr.result;
+  };
 
-inp_img.onchange=function(){
-    let fr =new FileReader
-    fr.readAsDataURL(inp_img.files[0])
-    fr.onload=function(){
-        showimg.src=fr.result
-    }
+  showimg.style.display = "block";
+  showvideo.style.display = "none";
+};
 
+let date = document.getElementById("date");
 
-    showimg.style.display="block"
-    showvideo.style.display="none"
+var months = [
+  'January', 'February', 'March', 'April', 'May', 'June',
+  'July', 'August', 'September', 'October', 'November', 'December'
+];
 
+let de = new Date();
+let year = de.getFullYear();
+let mon = months[de.getMonth()];
+let day = de.getDate();
 
-}
-inp_vid.onchange=function(){
-    let fr =new FileReader
-    fr.readAsDataURL(inp_vid.files[0])
-    fr.onload=function(){
-        showvideo.src=fr.result
-    }
+let full_date = `${mon} ${day} ${year}`;
 
-
-    showvideo.style.display="block"
-    showimg.style.display="none"
-
-}
-
-let date=document.getElementById("date")
-
-var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
-
-let de = new Date()
-
-
-let year =de.getFullYear()
-let mon =months[de.getMonth()]
-let day =de.getDate()
-
-let full_date = `${mon} ${day},${year}`
-
-date.innerHTML=full_date
+date.innerHTML = full_date;
--- app2/views.py
+++ app2/views.py
@@ -8,40 +8,57 @@ from django.conf import settings
 
 
 # Create your views here.
-
-
 def blogs(request):
-    if request.method=="POST":
+    if request.method == "POST":
+        title = request.POST.get('title')
+        content = request.POST.get('blog')
 
-        title=request.POST.get('title')
-        content=request.POST.get('blog')
-        image=request.FILES['image']
+        # Get the image from the request
+        if 'image' in request.FILES:
+            image = request.FILES['image']
+            print('exist')
+        else:
+            image = None
+            print(image)
+        username = request.session.get('username')
+        profile = get_object_or_404(Profile, user__username=username)
 
-        post=Post(title=title,content=content,image=image)
-        post.save()
+        # Ensure the profile exists before creating the post
+        if profile:
+            custom_user_instance = profile.user
+            # Create a Post instance with the provided data
+            post = Post(title=title, content=content, author=custom_user_instance)
+
+            if image:
+                post.image = image  # Assign the image to the 'image' field
+
+            post.save()
+            # Handle further operations
+        else:
+            # Handle the case where the profile is not found
+            pass
+
     username = request.session.get('username')
     print(username)
-    profile = get_object_or_404(Profile, user__username=username) 
+    profile = get_object_or_404(Profile, user__username=username)
     image_url = None
     if profile.image:
         image_url = settings.MEDIA_URL + str(profile.image)
-
-
-    posts=Post.objects.all()
-    #post_image=posts.image
-    #post_title=posts.title
-    #post_content=posts.content
-    context={
-    'image':image_url,
-    "posts":posts,
-    
+    posts = Post.objects.all()
+    context = {
+        'image': image_url,
+        'posts': posts,
     }
-    return render(request,'blog.html',context=context)
+    return render(request, 'blog.html', context=context)
+
+def one_blog (request,id):
+    post=Post.objects.get(id=id)
+    print(post)
 
 
+    context={
+        "post":post
+    }
 
 
-def one_blog(request,id):
-
-
-    return render(request,'post.html')
+    return render(request,'post.html',context=context)
